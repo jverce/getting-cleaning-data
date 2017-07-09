90c69f83a2b8f220fc91e46e10545dddb453cef0
Jay Vercellone <jay.vercellone@toptal.com>
2017-07-09 15:33:06 -0300

    Filter columns and add activity labels

--- run_analysis.R
+++ run_analysis.R
@@ -18,28 +18,33 @@ headers <- read.csv("features.txt", header=F, sep="")
 headers <- as.character(headers[, 2])
 
 # Load test data
+test_activity_ids <- read.csv("test/y_test.txt", header=F, sep="")
 test_subject_ids <- read.csv("test/subject_test.txt", header=F, sep="")
-test_activity_labels <- read.csv("test/y_test.txt", header=F, sep="")
 test_data <- read.csv("test/X_test.txt", header=F, sep="")
 # Merge all test data
-test_data <- cbind(test_activity_labels, test_data, test_subject_ids)
+test_data <- cbind(test_activity_ids, test_subject_ids, test_data)
 
 # Load training data
+train_activity_ids <- read.csv("train/y_train.txt", header=F, sep="")
 train_subject_ids <- read.csv("train/subject_train.txt", header=F, sep="")
-train_activity_labels <- read.csv("train/y_train.txt", header=F, sep="")
 train_data <- read.csv("train/X_train.txt", header=F, sep="")
 # Merge all training data
-train_data <- cbind(train_activity_labels, train_data, train_subject_ids)
+train_data <- cbind(train_activity_ids, train_subject_ids, train_data)
 
 # Merge test and training data together
 data <- rbind(test_data, train_data)
-colnames(data) <- c("Activity.Label", headers, "Subject.ID")
 
 # Extract mean and std measurements
-means <- data[, grep("mean", colnames(data), ignore.case=T)]
-stds <- data[, grep("std", colnames(data), ignore.case=T)]
-data <- cbind(means, stds)
+# We also need to keep the activity and subject info
+colnames(data) <- c("Activity.ID", "Subject.ID", headers)
+means <- data[, grep("-mean\\(", colnames(data), ignore.case=T)]
+stds <- data[, grep("-std\\(", colnames(data), ignore.case=T)]
+data <- cbind(Activity.ID=data$Activity.ID, Subject.ID=data$Subject.ID, means, stds)
 
+# Use desriptive activity names
+activity_labels <- read.csv("activity_labels.txt", header=F, sep="")
+colnames(activity_labels) <- c("Activity.ID", "Activity.Label")
+data <- merge(data, activity_labels)
 
 # Take the user back to its original dir
 setwd("..")
